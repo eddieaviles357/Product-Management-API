107fda7d11c66f74dfb869e9da9f36fa6388f483
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-09 21:28:58 -0700

    schema updated, sql creating db and test db added

--- pm-schema.sql
+++ pm-schema.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE products (
   p_id INTEGER PRIMARY KEY AUTOINCREMENT,
   sku VARCHAR(6) NOT NULL UNIQUE,
@@ -7,17 +5,16 @@ CREATE TABLE products (
   p_description VARCHAR(255) NOT NULL,
   p_price NUMERIC(7,2) NOT NULL,
   p_image_url TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  category_id INTEGER NOT NULL REFERENCES categories,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    category VARCHAR(20) NOT NULL UNIQUE
+  id INTEGER PRIMARY KEY,
+  category VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE products_categories (
-  id SERIAL PRIMARY KEY,
+  PRIMARY KEY (product_id, category_id),
   product_id INTEGER NOT NULL REFERENCES products ON DELETE CASCADE,
-  category_id INTEGER NOT NULL REFERENCES categories ON DELETE CASCADE
+  category_id INTEGER NOT NULL REFERENCES categories ON DELETE CASCADE,
 );
