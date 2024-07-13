86206f6c3572e1ccd9c0272ce5054b91f24313e4
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-12 20:50:08 -0700

    fixed syntax errors

--- pm-seed.sql
+++ pm-seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO products (p_id, sku, p_name, p_description, p_price, p_image_url, created_at)
 VALUES 
-(1, MC10SSMM, Shirt, White short sleeve medium, '10.99' 'https://image.product-management.com/1283859', NOW()),
-(2, MC10LSLL, Shirt, White long sleeve large, '10.99' 'https://image.product-management.com/1283860', NOW()),
-(3, MC12SLMM, Shirt, Red short sleeve medium, '10.99' 'https://image.product-management.com/1283861', NOW());
+(1, 'MC10SSMM', 'Shirt', 'White short sleeve medium', '10.99', 'https://image.product-management.com/1283859', NOW()),
+(2, 'MC10LSLL', 'Shirt', 'White long sleeve large', '10.99', 'https://image.product-management.com/1283860', NOW()),
+(3, 'MC12SLMM', 'Shirt', 'Red short sleeve medium', '10.99', 'https://image.product-management.com/1283861', NOW());
 
 
 INSERT INTO categories ( id,category )
@@ -20,7 +20,7 @@ VALUES
 (11, 'black'),
 (12, 'red'),
 (13, 'yellow'),
-(14, 'blue').
+(14, 'blue'),
 (15, 'ss'),
 (16, 'ls');
 
@@ -28,7 +28,7 @@ INSERT INTO products_categories (product_id, category_id)
 VALUES
 (1, 10),
 (1, 5),
-(1, 15)
+(1, 15),
 (2, 10),
 (2, 6),
 (2, 16),
