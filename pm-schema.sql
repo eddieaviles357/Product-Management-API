7bbae96d020c171b6f822bf4313058f6ce79539d
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-12 20:49:29 -0700

    removed autoincrement keyword, sku increased to 8 characters

--- pm-schema.sql
+++ pm-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products (
-  p_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  sku VARCHAR(6) NOT NULL UNIQUE,
+  p_id INTEGER PRIMARY KEY,
+  sku VARCHAR(8) NOT NULL UNIQUE,
   p_name VARCHAR(30) NOT NULL,
   p_description VARCHAR(255) NOT NULL,
   p_price NUMERIC(7,2) NOT NULL,
@@ -16,5 +16,5 @@ CREATE TABLE categories (
 CREATE TABLE products_categories (
   PRIMARY KEY (product_id, category_id),
   product_id INTEGER NOT NULL REFERENCES products ON DELETE CASCADE,
-  category_id INTEGER NOT NULL REFERENCES categories ON DELETE CASCADE,
+  category_id INTEGER NOT NULL REFERENCES categories ON DELETE CASCADE
 );
