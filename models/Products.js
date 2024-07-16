6327d9bd38c3047b9f84cd134267c499119b0595
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-15 20:30:41 -0700

    getProduct static method added

--- models/Products.js
+++ models/Products.js
@@ -2,10 +2,25 @@ const db = require("../db.js");
 
 class Products {
   static async getProducts() {
-    const result = await db.query('SELECT sku FROM products');
+    const result = await db.query(`
+      SELECT * FROM products`);
 
     console.log('RESULSTS', result)
   }
+
+  static async getProduct(id) {
+    const result = await db.query(`
+      SELECT
+        sku,
+        p_name AS name,
+        p_description AS description,
+        p_price AS price,
+        p_image_url AS imageURL,
+        created_at AS createdAt
+      WHERE id = $1
+    `, [id]);
+  }
+
 }
 
 module.exports = Products;
