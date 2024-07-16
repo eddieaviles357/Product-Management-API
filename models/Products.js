8d25a6047e823f649009ff3c5c2383248ee4f971
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-15 20:33:54 -0700

    static method added to add product

--- models/Products.js
+++ models/Products.js
@@ -21,6 +21,14 @@ class Products {
     `, [id]);
   }
 
+  static async addProduct(product) {
+    const { sku, name, description, price, imageURL } = product;
+
+    const result = await db.query(`
+      INSERT INTO products (sku, p_name, p_description, p_price, p_image_url, created_at)
+      VALUES ($1, $2, $3, $4, $5, NOW())
+      `, [sku, name, description, price, imageURL])   
+  }
 }
 
 module.exports = Products;
