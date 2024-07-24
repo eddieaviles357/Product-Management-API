1dcf3fdb4e108a7cfd76b4ba049c514cfabebac8
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-23 21:56:05 -0700

    Errors for api

---
+++ AppError.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/** Product Management API error extends normal JS error so we can
+ *  add a status when we make an instance of it.
+ *
+ *  The error-handling middleware will return this.
+ */
+
+class AppError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+  
+/** 404 NOT FOUND error. */
+
+class NotFoundError extends AppError {
+  constructor(message = "Not Found") {
+    super(message, 404);
+  }
+}
+
+/** 401 UNAUTHORIZED error. */
+
+class UnauthorizedError extends AppError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+/** 400 BAD REQUEST error. */
+
+class BadRequestError extends AppError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+/** 403 BAD REQUEST error. */
+
+class ForbiddenError extends AppError {
+  constructor(message = "Bad Request") {
+    super(message, 403);
+  }
+}
+
+module.exports = {
+  AppError,
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+  ForbiddenError,
+};
