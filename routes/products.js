e26581106a3655ffde88b1374ff97a3ed7f7883e
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-23 20:43:22 -0700

    moved logic to controllers folder

--- routes/products.js
+++ routes/products.js
@@ -2,40 +2,23 @@
 
 /** Routes for Products. */
 
-// const express = require("express");
-// const router = express.Router();
-const router = require("express").Router();
-const Products = require("../models/Products");
-
-/** GET /products => { products }
- *
- * Returns { "products": [ {sku, name, description, price, imageURL, createdAt} ] }
- *
- **/
-
-router.get("/", async function (req, res, next) {
-  
-  try {
-    const productsList = await Products.getProducts();
-    return res.json({"products": productsList});
-  } catch (err) {
-    return next(err);
-  }
-});
-
-router.get("/:id", async function (req, res, next) {
-  const id = req.params.id;
-  const product = await Products.findProductById(id);
-
-  try {
-    return res.json({ product });
-  } catch (err) {
-    return next(err);
-  }
-});
+// const Products = require("../models/Products");
+const express = require("express");
+const {
+  getProducts,
+  getProductById
+} = require("../controllers/products");
+const router = require("express").Router({mergeParams: true});
 
 
+router
+  .route('/', )
+  .get(getProducts)
 
+router
+  .route('/:id')
+  .get(getProductById)
 
 
 module.exports = router;
+
