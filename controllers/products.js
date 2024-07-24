5ca6ffa8dd6f129a47127f1ece93e1a7a4067d36
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-23 20:42:36 -0700

    controller added for products

---
+++ controllers/products.js
@@ -0,0 +1,35 @@
+const Products = require("../models/Products");
+
+// @desc      Get all products
+// @route     GET /api/v1/products
+// @access    Private/Admin ?????????
+exports.getProducts = async (req, res, next) => {
+  try {
+    const productsList = await Products.getProducts();
+
+    return res.status(200).json({ 
+      success: true, 
+      products: productsList
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// @desc      Get single products by id
+// @route     GET /api/v1/products/id
+// @access    Private/Admin ?????????
+exports.getProductById = async (req, res, next) => {
+  const id = req.params.id;
+  const product = await Products.findProductById(id);
+
+  try {
+    
+    return res.status(200).json({ 
+      success: true, 
+      product 
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
