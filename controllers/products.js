bc7a783b985da5f01a7bdc3818316bb70006a3b8
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-26 22:17:51 -0700

    2 new routes that add a product using a body from client, second route is removing a product object from db using id

--- controllers/products.js
+++ controllers/products.js
@@ -19,11 +19,11 @@ exports.getProducts = async (req, res, next) => {
 };
 
 // @desc      Get single products by id
-// @route     GET /api/v1/products/id
+// @route     GET /api/v1/products/:id
 // @access    Private/Admin ?????????
 exports.getProductById = async (req, res, next) => {
   try {
-    const id = req.params.id;
+    const id = Number(req.params.id)
     const product = await Products.findProductById(id);
     
     return res.status(200).json({ 
@@ -35,3 +35,34 @@ exports.getProductById = async (req, res, next) => {
   }
 };
 
+// @desc      Add product to db
+// @route     POST /api/v1/products
+// @access    Private/Admin ?????????
+exports.addProduct = async (req, res, next) => {
+  try {
+    const productToAdd = req.body;
+    await Products.addProduct(productToAdd);
+    
+    return res.status(200).json({ 
+      success: true, 
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// @desc      Delete product from db
+// @route     DELETE /api/v1/products/:id
+// @access    Private/Admin ?????????
+exports.removeProductById = async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    const { success, product_name: productName } = await Products.removeProduct(id);
+    return res.status(200).json({ 
+      success,
+      productName
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
