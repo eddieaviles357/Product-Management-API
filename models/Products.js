b7e867a66261dce17ee7ab5149e4662585c0dbd4
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-29 20:26:57 -0700

    minor white space movement

--- models/Products.js
+++ models/Products.js
@@ -9,14 +9,15 @@ const {
   ForbiddenError,
   UnprocessableEntityError
 } = require("../AppError.js");
-/*
-gets all products, ** LIMIT TO BE SET FOR PAGINATION **
-returns [
-  {sku, name, description, price, imageURL, createdAt}, 
-  {...}
-  ]
-*/
+
 class Products {
+  /*
+    gets all products, ** LIMIT TO BE SET FOR PAGINATION **
+    returns [
+      {sku, name, description, price, imageURL, createdAt}, 
+      {...}
+      ]
+  */
   static async getProducts() {
     const result = await db.query(`
       SELECT 
@@ -32,11 +33,11 @@ class Products {
     return (result.rows.length === 0) ? [] : result.rows;
   }
 
-/*
-gets product using an id
-returns { sku, name, description, price, imageURL, createdAt }
+  /*
+  gets product using an id
+  returns -> { sku, name, description, price, imageURL, createdAt }
 
-*/
+  */
   static async findProductById(id) {
     if(typeof id !== "number" || isNaN(id)) return new UnprocessableEntityError("ID must be a number");
 
@@ -57,8 +58,7 @@ returns { sku, name, description, price, imageURL, createdAt }
   }
 
   /*
-  adds a new product to db
-
+  adds a new product to db -> void
   */
   static async addProduct(product) {
     const { sku, name, description, price, imageURL } = product;
@@ -73,11 +73,11 @@ returns { sku, name, description, price, imageURL, createdAt }
 
   /*
   removes a product from db
-
+  returns -> { product_name, success }
   */
   static async removeProduct(id) {
     if(typeof id !== "number" || isNaN(id)) return new UnprocessableEntityError("ID must be a number");
-    
+
     const result = await db.query(`
       DELETE FROM products 
       WHERE product_id = $1
