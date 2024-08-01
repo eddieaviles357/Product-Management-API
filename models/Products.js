e00360a0c0803b0d68011e98c31bddf3f63ecd6f
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-07-31 22:46:45 -0700

    updateProduct will now return updated data and will account with same data input

--- models/Products.js
+++ models/Products.js
@@ -67,39 +67,51 @@ class Products {
       VALUES ($1, $2, $3, $4, $5, NOW())
       `, [sku, name, description, price, imageURL]);
       
-    console.log(result)
   }
 
   /*
   updates product
   */
   static async updateProduct(id, product) {
-    const { sku, name, description, price, imageURL } = product;
-    console.log("PRODUCT", product)
+    const { 
+      sku, 
+      name, 
+      description, 
+      price, 
+      imageURL 
+    } = product;
+
     const productInDb = JSON.stringify(await this.findProductById(id));
-    console.log(`productInDb =>>> ${productInDb}`)
+    const parsedProduct = JSON.parse(productInDb);
 
-    if(Object.keys(productInDb).length === 0) return {};
-    const {sku: sk, name: nm, description: desc, price: prc, imageURL: imgURL} = productInDb;
-
+    if(Object.keys(parsedProduct).length === 0) return {};
+    const {
+      sku: sk, 
+      name: nm, 
+      description: desc, 
+      price: prc, 
+      image_url: imgURL
+    } = parsedProduct;
+    console.log("IMAGEURL",imgURL)
+    
     const result = await db.query(`
       UPDATE products SET
-        sku = COALESCE (NULLIF($1, ''),$6)
-        product_name = COALESCE (NULLIF($2, ''),$7)
-        product_description = COALESCE (NULLIF($3, ''),$8)
-        price = COALESCE(NULLIF($4, ''),$9)
-        image_url = COALESCE(NULLIF($5, ''),$10)
+        sku = COALESCE( NULLIF( $1, '' ),$6 ),
+        product_name = COALESCE( NULLIF( $2, '' ),$7 ),
+        product_description = COALESCE( NULLIF( $3, '' ),$8 ),
+        price = COALESCE( NULLIF( $4, 0.00 ),$9 ),
+        image_url = COALESCE( NULLIF( $5, '' ),$10 )
       WHERE product_id = $11
       RETURNING 
         sku, 
         product_name AS productName,
-        product_description AS productDescription
+        product_description AS productDescription,
         price,
         image_url AS imageURL
-      `, [sku, name, description, price, imageURL, 
-          sk, nm, desc, prc, imgURL, id]);
-      
-    console.log("RESULT FROM udpateProduct function", result)
+      `, [sku, name, description, Number(price), imageURL, 
+        sk, nm, desc, prc, imgURL, id]);
+
+      return result.rows[0]
   }
 
   /*
