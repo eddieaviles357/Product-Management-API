ad4d578fedfcfabbdd7997e14ab209556a73ad00
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-08-14 20:56:37 -0700

    returns an empty array if no categories exist, if any categories exist we must map the array and extract the values

--- models/Categories.js
+++ models/Categories.js
@@ -4,10 +4,19 @@ const db = require("../db.js");
 
 class Categories {
 
+    /*
+    gets all categories, ** LIMIT TO BE SET FOR PAGINATION **
+    returns [
+      {category: 'category'},
+      {...}
+      ]
+  */
   static async getAllCategories() {
     const allCategories = await db.query(`SELECT category FROM categories`);
-    console.log(allCategories)
+
+    return ( allCategories.rows === 0 ) ? [] : allCategories.rows.map( c => c.category );
   }
+  
 }
 
 module.exports = Categories;
