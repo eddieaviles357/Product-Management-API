cf28026d2864cee5bc70764fb32326ef16c7f09f
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-08-19 21:45:12 -0700

    validate updated category

---
+++ middleware/validateNewCategory.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const jsonSchema = require("jsonschema")
+const newCategorySchema = require("../schemas/newCategorySchema.json")
+
+const validateNewCategory = (req, res, next) => {
+
+  const validatedSchema = jsonSchema.validate(req.body, newCategorySchema);
+  
+  if(!validatedSchema.valid) {
+    console.log(validatedSchema);
+    return res.status(400).json({ 
+      errors: validatedSchema.errors.map( errors => ({ property: errors.property.slice(9), message: errors.stack.slice(9) }))
+    });
+  }
+
+  next();
+}
+  
+module.exports = validateNewCategory;
