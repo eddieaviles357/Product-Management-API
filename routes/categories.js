e38f84358ea645fb1bf1e5747a9470943f76870f
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-08-19 21:45:50 -0700

    typo for declaring variable

--- routes/categories.js
+++ routes/categories.js
@@ -5,7 +5,7 @@ const {
 } = require("../controllers/categories");
 const router = require("express").Router({ mergeParams: true });
 const validateNewCategory = require("../middleware/validateNewCategory");
-cont validateUpdatedCategory = require("../middleware/validateUpdatedCategory");
+const validateUpdatedCategory = require("../middleware/validateUpdatedCategory");
 
 // createCategory,
 // updateCategory,
