4d52d714b1268a821c596943b417bc356a9655a4
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-08-21 20:48:05 -0700

    when adding a category, our db will lower case the new category to avoid ambiguity

--- models/Categories.js
+++ models/Categories.js
@@ -7,14 +7,14 @@ class Categories {
     /*
     gets all categories, ** LIMIT TO BE SET FOR PAGINATION **
     returns [
-      {category: 'category'},
+      {id: 1, category: 'category'},
       {...}
       ]
   */
   static async getAllCategories() {
-    const allCategories = await db.query(`SELECT category FROM categories`);
+    const allCategories = await db.query(`SELECT id, category FROM categories`);
 
-    return ( allCategories.rows === 0 ) ? [] : allCategories.rows.map( c => c.category );
+    return ( allCategories.rows === 0 ) ? [] : allCategories.rows.map( c => ({id: c.id, category: c.category}) );
   }
 
   static async addCategory(newCategory) {
@@ -22,11 +22,35 @@ class Categories {
 
     const result = await db.query(`
       INSERT INTO categories (category)
-      VALUES ($1)
-      `, [newCategory])
+      VALUES ( LOWER($1) )
+      `, [newCategory]);
+
     console.log(result)
     console.log(`Successfully added ${newCategory} to DB`);
   }
+
+  static async getCategoryId(category) {
+    if(typeof category !== 'string') return new Error("Must be a string");
+
+    const result = await db.query(`
+      SELECT id FROM categories WHERE category = $1
+      `, [category]);
+
+      console.log(result);
+
+      return result.rows[0]
+  }
+
+  // static async updateCategory(category) {
+    
+  //   if(typeof category !== 'string') return new Error('Must be a string');
+
+  //   const result = await db.query(`
+  //     UPDATE categories 
+  //     SET category = COALESCE( NULLIF( $1, '' ),$6 )
+  //     WHERE category = 
+  //     `, [category]);
+  // }
   
 }
 
