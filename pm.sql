afbcb5b14080ab93946fae4fbe17f364e2129218
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-09-09 20:43:11 -0700

    data was accidently removed;

---
+++ pm.sql
@@ -0,0 +1,17 @@
+\echo 'Delete and recreate product_management db?'
+\prompt 'Return for yes or control-c to cancel > ' foo
+
+DROP DATABASE IF EXISTS product_management;
+CREATE DATABASE product_management;
+\c product_management
+
+\i pm-schema.sql
+\i pm-seed.sql
+
+\echo 'Delete and recreate product_management_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE IF EXISTS product_management_test;
+CREATE DATABASE product_management_test;
+\connect product_management_test
+
