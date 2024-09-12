e7e0d03bb82220eb374f0c1c2d6de3fc0f4958cf
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-09-11 23:00:56 -0700

    updated query that returns all products associated with query and includes a list of categories it belongs to

--- models/Categories.js
+++ models/Categories.js
@@ -56,39 +56,59 @@ class Categories {
   }
   
   static async getAllCategoryProducts(category) {
-    const result = await db.query(`
-      SELECT 
-        p.product_id AS id,
-        p.sku,
-        p.product_name AS "productName",
-        p.product_description AS "productDescription",
-        p.price,
-        p.image_url AS "imageURL",
-        p.created_at AS "createdAt",
-        p.updated_at AS "updatedAt",
-        ARRAY_AGG(c.category) AS categories
-      FROM (SELECT 
-              product_id, 
-              sku, 
-              product_name, 
-              product_description, 
-              price, 
-              image_url, 
-              created_at, 
-              updated_at
-            FROM products LIMIT 20) AS p
-      JOIN products_categories pc ON pc.product_id = p.product_id
-      JOIN categories c ON c.id = pc.category_id
-      WHERE (c.category = $1)
-      GROUP BY 
-        p.product_id,
-        p.sku,
-        p.product_name,
-        p.product_description,
-        p.price,
-        p.image_url,
-        p.created_at,
-        p.updated_at
+    // const result = await db.query(`
+    //   SELECT 
+    //     p.product_id AS id,
+    //     p.sku,
+    //     p.product_name AS "productName",
+    //     p.product_description AS "productDescription",
+    //     p.price,
+    //     p.image_url AS "imageURL",
+    //     p.created_at AS "createdAt",
+    //     p.updated_at AS "updatedAt",
+    //     ARRAY_AGG(c.category) AS categories
+    //   FROM (
+    //         SELECT 
+    //           prod.product_id AS id,
+    //           prod.sku,
+    //           prod.product_name AS "productName",
+    //           prod.product_description AS "productDescription",
+    //           prod.price,
+    //           prod.image_url AS "imageURL",
+    //           prod.created_at AS "createdAt",
+    //           prod.updated_at AS "updatedAt"
+    //         FROM products prod
+    //         JOIN products_categories pc ON pc.product_id = prod.product_id
+    //         JOIN categories c ON c.id = pc.category_id
+    //         WHERE c.category = $1
+    //         GROUP BY prod.product_id
+    //       ) AS p
+    //   JOIN products_categories pc ON pc.product_id = p.product_id
+    //   JOIN categories c ON c.id = pc.category_id`, [category]);
+
+      const result = await db.query(`
+        WITH all_product_id AS (
+          SELECT p.product_id 
+          FROM products p 
+          JOIN products_categories pc ON pc.product_id = p.product_id 
+          JOIN categories c ON c.id = pc.category_id 
+          WHERE c.category = $1
+          ) 
+        SELECT 
+          prod.product_id AS id,
+          prod.sku,
+          prod.product_name AS "productName",
+          prod.product_description AS "productDescription",
+          prod.price,
+          prod.image_url AS "imageURL",
+          prod.created_at AS "createdAt",
+          prod.updated_at AS "updatedAt",
+          ARRAY_AGG(cat.category) AS categories 
+        FROM products prod 
+        JOIN products_categories p_c ON p_c.product_id = prod.product_id 
+        JOIN categories cat ON cat.id = p_c.category_id 
+        WHERE prod.product_id IN (SELECT product_id FROM all_product_id) 
+        GROUP BY prod.product_id
         `, [category]);
 
     return result.rows;
