dad8fe720b3f4e22b162ee2a05b3a00310b7171c
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-09-13 21:17:47 -0700

    imported function to add category to product

--- routes/products.js
+++ routes/products.js
@@ -8,10 +8,12 @@ const {
   getProductById,
   updateProduct,
   deleteProductById,
+  addCategoryToProduct
 } = require("../controllers/products");
 const router = require("express").Router({mergeParams: true});
 const validateNewProduct = require("../middleware/validateNewProduct");
 const validateUpdatedProduct = require("../middleware/validateUpdatedProduct");
+// const validateCategoryToProduct = require("../middleware/validateCategoryToProduct");
 
 router
   .route('/', )
@@ -24,6 +26,9 @@ router
   .put(validateUpdatedProduct, updateProduct)
   .delete(deleteProductById)
 
+router
+  .route('/:productId/category/:categoryId')
+  .post(addCategoryToProduct)
 
 module.exports = router;
 
