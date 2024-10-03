ca505fb5f53a7a5b85b016dc43fae1b210926fd1
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-10-02 21:17:07 -0700

    updated helper function

--- helpers/removeAllSpecialCharacters.js
+++
@@ -1,8 +0,0 @@
-
-const removeAllSpecialChars = (word) => {
-  if(typeof word !== 'string') throw new Error('Must be a string');
-  
-  return word.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>{}\[\]\\\//\d/ /]/gi, '');
-}
-
-module.exports = removeAllSpecialChars
