3e8b36a5bcffed6583b3a2a9a1be00585096c2a0
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-10-16 20:09:43 -0700

    updated schema to allow integer less than or equal to 5

---
+++ middleware/validateNewReview.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const jsonSchema = require("jsonschema")
+const newReviewSchema = require("../schemas/newReviewSchema.json");
+
+const validateNewReview = (req, res, next) => {
+
+  const validatedSchema = jsonSchema.validate(req.body, newReviewSchema);
+  
+  if(!validatedSchema.valid) {
+    console.log(validatedSchema);
+    return res.status(400).json({ 
+      errors: validatedSchema.errors.map( errors => ({ property: errors.property.slice(9), message: errors.stack.slice(9) }))
+    });
+  }
+
+  next();
+}
+  
+module.exports = validateNewReview;
