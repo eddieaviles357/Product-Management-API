2f8ac6dad09697bb79c9a2188935cb458436bbb9
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-10-25 21:30:21 -0700

    refactored code, updated return values

--- models/Categories.js
+++ models/Categories.js
@@ -14,28 +14,34 @@ class Categories {
       ]
   */
   static async getAllCategories(id = 100000000) {
-    const allCategories = await db.query(`
-      SELECT id, category 
-      FROM categories 
-      WHERE id < $1
-      ORDER BY id DESC
-      LIMIT 20
-      `, [id]);
-      return ( allCategories.rows === 0 ) ? [] : allCategories.rows;
+    const queryStatement = `SELECT id, category 
+                          FROM categories 
+                          WHERE id < $1
+                          ORDER BY id DESC
+                          LIMIT 20`;
+      const result = await db.query(queryStatement, [id]);
+      return (result.rows.length === 0) ? [] : result.rows;
   }
 
+  /*
+    add a category
+    returns {id, category}
+  */
   static async addCategory(newCategory) {
     newCategory = removeNonAlphabeticChars(newCategory);
 
-    const result = await db.query(`
-      INSERT INTO categories (category)
-      VALUES ( LOWER($1) )
-      RETURNING id, category
-      `, [newCategory]);
-      
+    const queryStatement = `INSERT INTO categories (category)
+                            VALUES ( LOWER($1) )
+                            RETURNING id, category`;
+    const result = await db.query(queryStatement, [newCategory]);
+    if(result.rows.length === 0) throw new BadRequestError("Something went wrong");
     return result.rows[0];
   }
 
+  /*
+    updates a category
+    returns {id, category}
+  */
   static async updateCategory(catId, updatedCategory) {
     
     // something aint right lets just return an error ❌
@@ -45,70 +51,62 @@ class Categories {
     updatedCategory = removeNonAlphabeticChars(updatedCategory);
 
     // does category exist in db 🤔
-    const catExist = await db.query(`
-      SELECT id, category
-      FROM categories 
-      WHERE id = $1
-      `, [catId]);
+    const doesCategoryExistStatement = `SELECT id, category
+                                        FROM categories 
+                                        WHERE id = $1`
+    const categoryExistQuery = await db.query(doesCategoryExistStatement, [catId]);
 
     // doesn't exist in db lets return error ❌
-    if(catExist.rows.length === 0) throw new BadRequestError(`${updatedCategory} does not exist`);
-    const { category } = catExist.rows[0];
+    if(categoryExistQuery.rows.length === 0) throw new BadRequestError(`${updatedCategory} does not exist`);
+    const { category } = categoryExistQuery.rows[0];
 
     if(category === updatedCategory) throw new BadRequestError(`${updatedCategory} already exists`);
-
-    const result = await db.query(`
-      UPDATE categories 
-      SET category = COALESCE(NULLIF($1, ''), $2)
-      WHERE id = $3
-      RETURNING id, category
-      `, [updatedCategory, category, catId]);
+    const updateQueryStatement = `UPDATE categories 
+                                  SET category = COALESCE(NULLIF($1, ''), $2)
+                                  WHERE id = $3
+                                  RETURNING id, category`;
+    const updateQueryValues = [updatedCategory, category, catId];
+    const result = await db.query(updateQueryStatement, updateQueryValues);
 
     return result.rows[0];
   }
   
   static async getAllCategoryProducts(catId) {
-
-      const result = await db.query(`
-        WITH all_product_id AS (
-          SELECT p.product_id 
-          FROM products p 
-          JOIN products_categories pc ON pc.product_id = p.product_id 
-          JOIN categories c ON c.id = pc.category_id 
-          WHERE c.id = $1
-          LIMIT 20
-          ) 
-        SELECT 
-          prod.product_id AS id,
-          prod.sku,
-          prod.product_name AS "productName",
-          prod.product_description AS "productDescription",
-          prod.price,
-          prod.stock,
-          prod.image_url AS "imageURL",
-          prod.created_at AS "createdAt",
-          prod.updated_at AS "updatedAt",
-          ARRAY_AGG(cat.category) AS categories 
-        FROM products prod 
-        JOIN products_categories p_c ON p_c.product_id = prod.product_id 
-        JOIN categories cat ON cat.id = p_c.category_id 
-        WHERE prod.product_id IN (SELECT product_id FROM all_product_id) 
-        GROUP BY prod.product_id
-        `, [catId]);
-
-    return result.rows;
+    const queryStatement = `WITH all_product_id AS 
+                              ( SELECT p.product_id 
+                                FROM products p 
+                                JOIN products_categories pc ON pc.product_id = p.product_id 
+                                JOIN categories c ON c.id = pc.category_id 
+                                WHERE c.id = $1
+                                LIMIT 20 ) 
+                            SELECT 
+                              prod.product_id AS id,
+                              prod.sku,
+                              prod.product_name AS "productName",
+                              prod.product_description AS "productDescription",
+                              prod.price,
+                              prod.stock,
+                              prod.image_url AS "imageURL",
+                              prod.created_at AS "createdAt",
+                              prod.updated_at AS "updatedAt",
+                              ARRAY_AGG(cat.category) AS categories 
+                            FROM products prod 
+                            JOIN products_categories p_c ON p_c.product_id = prod.product_id 
+                            JOIN categories cat ON cat.id = p_c.category_id 
+                            WHERE prod.product_id IN (SELECT product_id FROM all_product_id) 
+                            GROUP BY prod.product_id`;
+    const result = await db.query(queryStatement, [catId]);
+    return (result.rows.length === 0) ? [] : result.rows;
   }
   
   static async removeCategory(catId) {
-
-    const result = await db.query(`
-      DELETE FROM categories WHERE id = $1
-      RETURNING category
-    `, [catId]);
+    const queryStatement = `DELETE FROM categories WHERE id = $1
+                            RETURNING category`;
+    const result = await db.query(queryStatement, [catId]);
     
     return (result.rows.length === 0) 
-    ? { category: `Category with id ${catId} not found`, success: false } 
-    : { category: result.rows[0], success: true }
+            ? { category: `Category with id ${catId} not found`, success: false } 
+            : { category: result.rows[0], success: true }
   }
 }
 
