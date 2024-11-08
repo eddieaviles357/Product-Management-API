4131095dd06ee0f5602f5c2a9adba2f9199f1aa1
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-11-07 21:01:10 -0800

    updated query return values, updated error handling. An object with error message will not be returned

--- models/Users.js
+++ models/Users.js
@@ -4,23 +4,35 @@ const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
+const {
+  NotFoundError,
+  BadRequestError,
+//   UnauthorizedError,
+//   BadRequestError,
+//   ForbiddenError,
+//   UnprocessableEntityError
+} = require("../AppError.js");
 
 class Users {
 
   static async register({firstName, lastName, username, password, email, isAdmin=false}) {
     try {
-      const queryStatement = `INSERT INTO users (first_name, last_name, username, password, email, is_admin
+      // create salt and hash password, we will store this in db
+      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+      const queryStatement = `INSERT INTO users (first_name, last_name, username, password, email, is_admin)
                               VALUES 
                                 ($1, $2, $3, $4, $5, $6)
-                              RETURNING *`;
-      const queryValues = [firstName, lastName, username, password, email, isAdmin];
+                              RETURNING 
+                                id, first_name AS "firstName", last_name AS "lastName", username, email`;
+      const queryValues = [firstName, lastName, username, hashedPassword, email, isAdmin];
       // hash password
-      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
       // store user in database
       const result = await db.query(queryStatement, queryValues);
-      console.log(result.rows[0]);
+      if(result.rows.length === 0) throw new BadRequestError('Invalid, something went wrong');
+      return result.rows[0];
     } catch (err) {
-      console.log("ERROR\n", err);
+      return { error: err.message }
     }
   }
 };
