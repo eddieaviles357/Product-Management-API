c2c256f84b75b02a0ab5858526354d26c4d5b70f
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-11-12 21:08:16 -0800

    validation schema and validation middleware added when registering a new user to db

---
+++ middleware/validateNewUser.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const jsonSchema = require("jsonschema")
+const newUserSchema = require("../schemas/newUserSchema.json");
+
+const validateNewUser = (req, res, next) => {
+
+  const validatedSchema = jsonSchema.validate(req.body, newUserSchema);
+  
+  if(!validatedSchema.valid) {
+    console.log(validatedSchema);
+    return res.status(400).json({ 
+      errors: validatedSchema.errors.map( errors => ({ property: errors.property.slice(9), message: errors.stack.slice(9) }))
+    });
+  }
+
+  next();
+}
+  
+module.exports = validateNewUser;
