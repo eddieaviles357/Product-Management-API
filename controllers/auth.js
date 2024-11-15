38abc47ce2cf5839bb31754ccdc331fde53897cd
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-11-14 22:27:51 -0800

    import create token, when registering a user the return value will be the user data and a token, when authenticating user return value will be the user data and token

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,20 +2,37 @@
 
 const Users = require("../models/Users");
 const { BadRequestError } = require("../AppError");
+const createToken = require("../helpers/tokens");
 
 // @desc      Registers a new user to the database
-// @route     POST /api/v1/users
+// @route     POST /api/v1/auth
 // @access    Private/Admin ?????????
 exports.registerUser = async (req, res, next) => {
   try {
     const body = req.body
-    const result = await Users.register(body);
+    const newUser = await Users.register(body);
+    const token = createToken(newUser);
 
-    return res.status(200).json({
-      success: true,
-      result
+    return res.status(201).json({ 
+      result: newUser, 
+      token 
     });
     } catch(err) {
       return next(err);
     }
   };
+
+// @desc      Creates a token when user is autherized
+// @route     POST /api/v1/auth/token
+// @access    Private/Admin ?????????
+exports.token = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.authenticate(username, password);
+    const token = createToken(user);
+    
+    return res.json({ result: user, token });
+  } catch (err) {
+    return next(err);
+  }
+}
