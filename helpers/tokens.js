8345b1888d6bf7cc1933a18f764618ca08969921
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-11-16 22:19:11 -0800

    assert will console log in debugger when isAdmin property is not detected

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -5,13 +5,14 @@ const { SECRET_KEY } = require("../config");
 
 // return signed JWT token from user data
 const createToken = (user) => {
+  console.assert(user.isAdmin !== undefined, "create token passed without isAdmin property");
+  
+  const payload = {
+    username: user.username,
+    isAdmin: user.isAdmin || false
+  };
 
-const payload = {
-  username: user.username,
-  isAdmin: user.isAdmin || false
-};
-
-return jwt.sign(payload, SECRET_KEY);
+  return jwt.sign(payload, SECRET_KEY);
 }
 
 module.exports = createToken;
