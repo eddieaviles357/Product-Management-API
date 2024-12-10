8d137a7ee257c0b3f40567760a9f51fefa5861ab
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-12-09 21:03:01 -0800

    class updated to Wishlist, removing a product form wishlist has been added;

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -7,8 +7,8 @@ const {
   UnauthorizedError
 } = require("../AppError.js");
 
-class Users {
-
+class Wishlist {
+  
   static async addProduct(username, productId) {
     try {
       // check if username exist, if so get the id reference
@@ -25,6 +25,28 @@ class Users {
       throw new BadRequestError(err.message);
     }
   }
+
+  static async removeProduct(username, productId) {
+    try {
+      // check if username exist, if so get the id reference
+      const userResult = await db.query(`SELECT id FROM users WHERE username = $1`, [username]);
+      if(userResult.rows.length === 0) throw new BadRequestError(`User ${username} does not exist`);
+      const userId = userResult.rows[0].id;
+      
+      // Delete product to wishlist using user id and procuct id
+      const queryStatement = `DELETE FROM wishlist 
+                              WHERE user_id = $1 AND product_id = $2
+                              RETURNING 
+                                user_id AS "userId",
+                                product_id AS "productId"`;
+      const values = [userId, productId];
+      const wishlistValues = await db.query(queryStatement, values);
+
+      return wishlistValues.rows[0];
+    } catch (err) {
+      throw new BadRequestError(err.message);
+    }
+  }
 };
 
-module.exports = Users;
+module.exports = Wishlist;
