dfe7648aa0baf814ab86a8d557fb56193bc47d48
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-12-09 21:01:43 -0800

    deleteWishlist route added for removing a user wishlist

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -5,12 +5,26 @@ const { BadRequestError } = require("../AppError");
 
 
 // @desc      Add product to wishlist
-// @route     POST /api/v1/wishlist/:username/addToWishlist/:productId
+// @route     POST /api/v1/wishlist/:username/:productId
 // @access    Private/Admin ?????????
 exports.addToWishlist = async (req, res, next) => {
   try {
     const { username, productId } = req.params;
     const wishlist = await Wishlist.addProduct(username, productId);
+
+    return res.status(200).json({success: true, wishlist});
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// @desc      Deletes product to wishlist
+// @route     DELETE /api/v1/wishlist/:username/:productId
+// @access    Private/Admin ?????????
+exports.deleteToWishlist = async (req, res, next) => {
+  try {
+    const { username, productId } = req.params;
+    const wishlist = await Wishlist.removeProduct(username, productId);
 
     return res.status(200).json({success: true, wishlist});
   } catch (err) {
