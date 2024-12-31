46226750897aa6c7dd27474b62c77d5fd57b8eca
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-12-30 21:45:59 -0800

    if user sends request with no body we will assign quantity value to undefined

--- controllers/cart.js
+++ controllers/cart.js
@@ -5,7 +5,7 @@ const Cart = require("../models/Cart");
 exports.addToCart = async (req, res, next) => {
   try {
     const {username, productId} = req.params;
-    const {quantity} = req.body;
+    const {quantity} = req.body ?? undefined;
     const result = await Cart.addToCart(username, productId, quantity);
 
     res.status(200).json({success: true, result})
