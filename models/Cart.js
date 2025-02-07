a38ebabb91e425aa14a2932e5380a0f7b3c6d123
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-02-06 20:22:51 -0800

    _getPrice private method added to reduce redundant code

--- models/Cart.js
+++ models/Cart.js
@@ -5,7 +5,16 @@ const {BadRequestError} = require("../AppError");
 const getUserId = require("../helpers/getUserId");
 
 class Cart {
+  static async _getPrice(productId) {
+    // get product price
+    const result = await db.query(`SELECT price FROM products WHERE product_id = $1`, [productId]);
+    if(result.rows.length === 0) throw new BadRequestError(`Item ${productId} does not exist`);
+    // price from db is returned as a string we have to cast to Int
+    return Number(result.rows[0].price);
+  }
 
+  // Returns [{ id: Int, userId: Int, productId: Int, quantity: Int, price: String, addedAt: Date, updatedAt: Date}]
+  // or []
   static async get(username) {
     try {
       const userId = await getUserId(username);
@@ -21,12 +30,13 @@ class Cart {
                               FROM cart
                               WHERE user_id = $1`;
       const cartResult = await db.query(queryStatement, [userId]);
-      return (cartResult.rows.length === 0) ? {} : cartResult.rows;
+      return (cartResult.rows.length === 0) ? [] : cartResult.rows;
     } catch (err) {
       throw new BadRequestError(err.message);
     }
   }
 
+  // Returns Boolean
   static async clear (username) {
     try {
       const userId = await getUserId(username);
@@ -39,15 +49,13 @@ class Cart {
     }
   }
 
+  // returns { user_id: Int, product_id: Int, quantity: Int }
   static async addToCart(username, productId, quantity = 1) {
     try {
       let price;
       const userId = await getUserId(username);
 
-      // get price from product we will use this to total up price
-      const priceResult = await db.query(`SELECT price FROM products WHERE product_id = $1`, [productId]);
-      if(priceResult.rows.length === 0) throw new BadRequestError(`Item ${productId} does not exist`);
-      price = Number(priceResult.rows[0].price);
+      price = await Cart._getPrice(productId);
 
       // We are only going to allow one product per cart. We will have to use updateCartItemQty to increase the quantity
       const cartItemResult = await db.query(`SELECT user_id, product_id, quantity FROM cart WHERE user_id = $1 AND product_id = $2`, [userId, productId]);
@@ -66,16 +74,15 @@ class Cart {
     }
   }
 
+  // if item Qty is 0 then it will be removed from the cart and will return []
+  // returns [ { user_id: Int, product_id: Int, quantity: Int, price: String } ]
   static async updateCartItemQty(username, productId, quantity = 0) {
     try {
       let price;
       let qty;
       const userId = await getUserId(username);
 
-      // get price from product we will use this to total up price
-      const priceResult = await db.query(`SELECT price FROM products WHERE product_id = $1`, [productId]);
-      if(priceResult.rows.length === 0) throw new BadRequestError(`Item ${productId} does not exist`);
-      price = Number(priceResult.rows[0].price);
+      price = await Cart._getPrice(productId);
 
       // get cart item details, if no data exist throw error
       const itemResult = await db.query(`SELECT quantity FROM cart WHERE user_id = $1 AND product_id = $2`, [userId, productId]);
@@ -102,7 +109,7 @@ class Cart {
       throw new BadRequestError(err.message);
     }
   }
-
+  // return String | { id: Int }
   static async removeCartItem (username, productId) {
     try {
       const userId = await getUserId(username);
@@ -111,8 +118,8 @@ class Cart {
       const values = [userId, productId];
       const deleteResult = await db.query(queryStatement, values);
 
-      if(deleteResult.rows.length === 0) return 'Nothing to delete';
-      return deleteResult.rows;
+      return (deleteResult.rows.length === 0) ? 'Nothing to delete' : deleteResult.rows[0].id;
+
     } catch (err) {
       throw new BadRequestError(err.message);
     }
