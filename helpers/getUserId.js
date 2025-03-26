12efe77e3e760aaf5153e91b711d7ff06381949e
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-03-25 18:18:19 -0700

    try catch block added

--- helpers/getUserId.js
+++ helpers/getUserId.js
@@ -6,10 +6,14 @@ const {BadRequestError} = require("../AppError");
 // check if username already exists. If so, get the id reference
 // Returns user id type Number
 const getUserId = async (username) => {
-  const userResult = await db.query(`SELECT id FROM users WHERE username = $1`, [username]);
-
-  // return 0 is no user exist
-  return (userResult.rows.length === 0) ? 0 : userResult.rows[0].id
+  try {
+    const userResult = await db.query(`SELECT id FROM users WHERE username = $1`, [username]);
+  
+    // return 0 is no user exist
+    return (userResult.rows.length === 0) ? 0 : userResult.rows[0].id
+  } catch (err) {
+    throw new BadRequestError(err.message);
+  }
 }
 
 module.exports = getUserId;
