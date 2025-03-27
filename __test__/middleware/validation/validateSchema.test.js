a8161df709d6c0dcf2c3ae8d1b2907a785a769e2
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-03-26 20:04:43 -0700

    test for validation

---
+++ __test__/middleware/validation/validateSchema.test.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const { BadRequestError } = require("../../../AppError");
+const validateSchema = require("../../../middleware/validation/validateSchema");
+const userAuthSchema = require("../../../schemas/userAuthSchema.json");
+
+describe("validateSchema Middleware", () => {
+  let req, res, next;
+
+  beforeEach(() => {
+    req = { body: {} };
+    res = {
+      status: jest.fn().mockReturnThis(), // used to return this for chaining
+      json: jest.fn()
+    };
+    next = jest.fn();
+  });
+
+  test("should call next() if schema is valid", () => {
+    req.body = { username: "testuser", password: "Test@1234" };
+    
+    validateSchema(userAuthSchema)(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  test("should return 400 if schema is invalid", () => {
+    req.body = { username: "testuser" }; // Missing password field
+
+        validateSchema(userAuthSchema)(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      errors: expect.any(Array)
+    });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test("should return 400 and provide error message that requires password", () => {
+    req.body = { username: "testuser" }; // Missing password field
+
+    validateSchema(userAuthSchema)(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      errors: expect.arrayContaining([
+        expect.objectContaining({
+          property: expect.any(String),
+          message: "requires property \"password\""
+        })
+      ])
+    });
+    expect(next).not.toHaveBeenCalled();
+  }); 
+
+  test("should privide detail error for exceeding username length max 20 characters", () => {
+    req.body = { username: "morethan20+morethan20", password: "Test@1234" };
+// 20 password 60
+    validateSchema(userAuthSchema)(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      errors: expect.arrayContaining([
+        expect.objectContaining({
+          property: "username",
+          message: "username does not meet maximum length of 20"
+        })
+      ])
+    });
+    expect(next).not.toHaveBeenCalled();
+  }); 
+});
