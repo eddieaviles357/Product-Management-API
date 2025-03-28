72676ba160378fe6e5f5341f471a52ff81865e52
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-03-27 20:33:41 -0700

    test for helper function, removes any non Alpha Numeric Chars

---
+++ __test__/helpers/removeNonAlphaNumericChars.test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const removeNonAlphaNumericChars = require("../../helpers/removeNonAlphaNumericChars");
+
+describe("remove non-alphanumeric chars", () => {
+  test("works", () => {
+    // Should remove all non-alphanumeric characters from the string
+    const input = "This++123 should__123 remove123 #@* non-alphanumeric#@123 characters123";
+    const output = removeNonAlphaNumericChars(input);
+   
+    expect(output).toBe("This123should123remove123nonalphanumeric123characters123");
+  })
+});
