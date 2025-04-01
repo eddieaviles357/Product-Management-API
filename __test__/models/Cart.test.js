627f4e8b391605360dd6c5568513e3d13c21113d
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-03-31 20:37:04 -0700

    test for adding item to cart and throwing error

--- __test__/models/Cart.test.js
+++ __test__/models/Cart.test.js
@@ -45,5 +45,34 @@ describe("get cart using username", () => {
     } catch (err) {
       expect(err).toBeInstanceOf(BadRequestError); 
     }
-  });  
+  });
+  
+  describe("add item to cart", () => {
+    test("successfully adds item", async () => {
+      // expect.assertions(2)
+      const newItem = {
+        username: userIdUsername[0].username,
+        user_id: userIdUsername[0].id,
+        product_id: productIds[0],
+        quantity: 1
+      };
+      
+      // First check the cart before adding
+      let cartBefore = await Cart.get(newItem.username);
+      expect(cartBefore).not.toHaveLength(0); // ensure cart exists then add item to cart
+      
+      const addedItem = await Cart.addToCart(newItem.username, newItem.product_id, newItem.quantity);
+      expect(addedItem).toBeTruthy();
+
+      expect(addedItem).toEqual(expect.objectContaining( {user_id: newItem.user_id, product_id: newItem.product_id, quantity: newItem.quantity}));
+    });
+
+    test("throws error for invalid id", async () => {
+      const invalidItem = {
+        username: userIdUsername[0].username,
+        productId: productIds[0]
+      };
+      await expect(Cart.addToCart(invalidItem.username, 0)).rejects.toThrow("Item 0 does not exist");
+    });
+  });
 });
