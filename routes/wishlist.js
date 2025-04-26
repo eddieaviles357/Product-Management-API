2a08e088be960622a484de726d6f03dfdfe02731
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-04-25 20:56:00 -0700

    getWishList route added

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -3,13 +3,15 @@
 const {
   addToWishlist,
   deleteToWishlist,
-  clearWishlist
+  clearWishlist,
+  getWishlist
 } = require("../controllers/wishlist");
 const router = require("express").Router();
 const {ensureLoggedIn, ensureUser} = require("../middleware/auth/auth");
 
 router
   .route("/:username")
+  .get(ensureLoggedIn, ensureUser, getWishlist)
   .delete(clearWishlist)
 
 router
