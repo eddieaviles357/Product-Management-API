e776c34a330362f15171b257f712a2a2f9e082a9
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-04-25 20:54:14 -0700

    getWishList method added, documentation added for all methods"

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -6,9 +6,67 @@ const { BadRequestError } = require("../AppError");
 const getUserId = require("../helpers/getUserId");
 
 class Wishlist {
-  
+  /**
+   * @param {string} username
+   * @param {string} username 
+   * @returns {array} wishlist
+   * @throws {BadRequestError} if username is not provided
+   * @throws {BadRequestError} if user does not exist or if there is an error in the query
+   * @throws {BadRequestError} if no products found in wishlist
+   */
+  static async getWishlist(username) {
+    try {
+      if (!username) throw new BadRequestError("Username is required");
+
+      const userId = await getUserId(username);
+
+      // Get all products in wishlist using user id
+      const getWishListQuery = `SELECT 
+                                p.product_id AS "productId",
+                                p.product_name AS "productName",
+                                p.price AS "productPrice",
+                                p.image_url AS "productImageUrl"
+                              FROM wishlist w
+                              JOIN products p ON w.product_id = p.product_id
+                              WHERE w.user_id = $1`;
+      const values = [userId];
+      const wishlistValues = await db.query(getWishListQuery, values);
+
+      if (wishlistValues.rows.length === 0) throw new BadRequestError("No products found in wishlist");
+      
+      return wishlistValues.rows;
+    } catch (err) {
+      throw new BadRequestError(err.message);
+    }
+  }
+  // static async getWishlistCount(username) {
+  //   try {
+  //     const userId = await getUserId(username);
+
+  //     // Get count of products in wishlist using user id
+  //     const queryStatement = `SELECT COUNT(*) AS "count" FROM wishlist WHERE user_id = $1`;
+  //     const values = [userId];
+  //     const countResult = await db.query(queryStatement, values);
+  //     const count = countResult.rows[0].count;
+
+  //     return count;
+  //   } catch (err) {
+  //     throw new BadRequestError(err.message);
+  //   }
+  // }
+
+  /**
+   * @param {string} username
+   * @param {number} productId
+   * @returns {object} wishlist item
+   * @throws {BadRequestError} if username or productId is not provided
+   * @throws {BadRequestError} if user does not exist or if there is an error in the query
+   * @throws {BadRequestError} if product already exists in wishlist
+   */
   static async addProduct(username, productId) {
     try {
+      if (!username) throw new BadRequestError("Username is required");
+      if (!productId) throw new BadRequestError("Product ID is required");
       const userId = await getUserId(username);
 
       // add product to wishlist using user id and product id
@@ -22,8 +80,18 @@ class Wishlist {
     }
   }
 
+  /**
+   * @param {string} username
+   * @param {number} productId
+   * @returns {boolean} true if product was removed from wishlist, false otherwise
+   * @throws {BadRequestError} if username or productId is not provided
+   * @throws {BadRequestError} if user does not exist or if there is an error in the query
+   * 
+   */
   static async removeProduct(username, productId) {
     try {
+      if (!username) throw new BadRequestError("Username is required");
+      if (!productId) throw new BadRequestError("Product ID is required");
       const userId = await getUserId(username);
       
       // Delete product from wishlist using user id and product id
@@ -43,6 +111,12 @@ class Wishlist {
     }
   }
 
+  /**
+   * @param {string} username
+   * @returns {boolean} true if wishlist was cleared, false otherwise
+   * @throws {BadRequestError} if username is not provided
+   * @throws {BadRequestError} if user does not exist or if there is an error in the query
+   */
   static async removeAll(username) {
     try {
       const userId = await getUserId(username);
