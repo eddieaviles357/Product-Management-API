82944d2497e224a6e0540511315dad7fa0f65c95
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-04-30 20:18:27 -0700

    validatedSchema will now return expected results, eg(no username field entered return property: username)

--- middleware/validation/validateSchema.js
+++ middleware/validation/validateSchema.js
@@ -5,10 +5,16 @@ const jsonSchema = require("jsonschema");
 
 const validateSchema = (schema) => (req, res, next) => {
   const validatedSchema = jsonSchema.validate(req.body, schema);
-  
+
   if(!validatedSchema.valid) {
+    const isPropertyRequired = validatedSchema?.errors[0]?.name === "required";
+    
     return res.status(400).json({
-      errors: validatedSchema.errors.map( errors => ({ property: errors.property.slice(9), message: errors.stack.slice(9) }))
+      errors: validatedSchema.errors.map( errors => (
+        { 
+          property: (isPropertyRequired) ? errors.argument : errors.property.slice(9), 
+          message: errors.stack.slice(9) }
+      ))
     });
   };
 
