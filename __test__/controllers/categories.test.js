2b20ff75a9f38fd33c93dc6010dab04b48d07539
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-05-10 21:26:39 -0700

    test include post route and put, still needs more coverage

--- __test__/controllers/categories.test.js
+++ __test__/controllers/categories.test.js
@@ -87,4 +87,134 @@ describe("Categories Routes", () => {
       });
     });
   });
+
+  describe("POST /categories", () => {
+    // ✅
+    test("creates a new category, works with admin privilage", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({ category: "newCategory" });
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toEqual({
+        success: true,
+        newCategory: {
+          id: expect.any(Number),
+          category: "newcategory", // category will be lowercase
+        }
+      });
+    });
+    // ✅
+    test("throws ConflictError when category already exists", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({ category: "none" });
+
+      expect(response.statusCode).toBe(409);
+      expect(response.body).toEqual({
+        error: { 
+          message: 'Review for this product already exists', 
+          status: 409
+        }
+      });
+    });
+    // ✅
+    test("throws BadRequestError when no category is provided", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({});
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        errors: [{
+          message: "requires property \"category\"",
+          property: "category",
+      }],
+      });
+    });
+    // ✅
+    test("throws BadRequestError when invalid data type is provided", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({ category: 12345 });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        errors: [{ 
+          message: "category is not of a type(s) string", 
+          property: "category"
+        }]
+      });
+    });
+    // ✅
+    test("throws BadRequestError when category is too long", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({ category: "a".repeat(21) });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        errors: [{ 
+          message: "category does not meet maximum length of 20", 
+          property: "category"
+        }]
+      });
+    });
+    // ✅
+    test("throws BadRequestError when user is not an admin", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .set("Authorization", `Bearer ${createToken({ username: username2 })}`)
+        .send({ category: "newCategory" });
+
+      expect(response.statusCode).toBe(401);
+      expect(response.body).toEqual({
+        error: { 
+          message: 'Unauthorized', 
+          status: 401
+        }
+      });
+    });
+    // ✅
+    test("throws BadRequestError when no token is provided", async () => {
+      const response = await request(app)
+        .post("/api/v1/categories")
+        .send({ category: "newCategory" });
+
+      expect(response.statusCode).toBe(401);
+      expect(response.body).toEqual({
+        error: { 
+          message: 'Unauthorized',
+          status: 401
+        }
+      });
+    });
+  });
+  describe("PUT /categories/:categoryId", () => {
+    // ✅
+    test("updates a category, works with admin privilage", async () => {
+      console.log("ID", categoryIds[0]);
+      const response = await request(app)
+        .put(`/api/v1/categories/${categoryIds[0]}`)
+        .set("Authorization", `Bearer ${createToken({ username: username1, isAdmin: true })}`)
+        .send({ category: "updatedCategory" });
+
+      expect(response.statusCode).toBe(200);
+      console.log("RESPONSE", response.body);
+      expect(response.body).toEqual({
+        success: true,
+        category: {
+          id: categoryIds[0],
+          category: "updatedcategory",
+        }
+      });
+    });
+
+  });
+  
 });
