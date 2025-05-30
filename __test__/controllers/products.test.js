c1c131106788c785d86c4d769e00a0c66ed7edb9
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-05-29 20:27:50 -0700

    test coverage for posting products

--- __test__/controllers/products.test.js
+++ __test__/controllers/products.test.js
@@ -2,6 +2,7 @@
 const request = require("supertest");
 const app = require("../../app");
 const Products = require("../../models/Products");
+const User = require("../../models/Users");
 const createToken = require("../../helpers/tokens");
 const { BadRequestError } = require("../../AppError");
 const db = require("../../db.js");
@@ -38,7 +39,6 @@ describe("Products Controller", () => {
     test("should return all products", async () => {
       const response = await request(app).get("/api/v1/products");
       const res = await db.query("select * from products")
-      console.log("Products in DB:", res.rows);
       expect(response.statusCode).toBe(200);
       expect(response.body.success).toBe(true);
       expect(response.body.products.length).toBeGreaterThan(0);
@@ -63,6 +63,98 @@ describe("Products Controller", () => {
     test("should return 400 for invalid id", async () => {
       const response = await request(app).get("/api/v1/products/invalid");
       expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe("POST /api/v1/products", () => {
+    test("should add a new product", async () => {
+      const newProduct = {
+        sku: "newsku",
+        name: "New Product",
+        description: "This is a new product",
+        price: 100,
+        stock: 50,
+        imageURL: "http://example.com/image.jpg"
+      };
+      const currentUser = await User.authenticate(username1, "password");
+      const token = createToken(currentUser);
+
+      const response = await request(app)
+        .post("/api/v1/products")
+        .send(newProduct)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body.success).toBe(true);
+      expect(response.body.product.productName).toBe(newProduct.name);
+    });
+
+    test("should return 401 for unauthorized user", async () => {
+      const newProduct = {
+        sku: "new-sku",
+        name: "New Product",
+        description: "This is a new product",
+        price: 100,
+        stock: 50,
+        imageURL: "http://example.com/image.jpg"
+      };
+
+      const response = await request(app)
+        .post("/api/v1/products")
+        .send(newProduct);
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    test("should return 400 for invalid product data", async () => {
+      const newProduct = {
+        sku: "newsku",
+        name: "New Product",
+        description: "This is a new product",
+        price: -100, // Invalid price
+        stock: 50,
+        imageURL: "http://example.com/image.jpg"
+      };
+      const currentUser = await User.authenticate(username1, "password");
+      const token = createToken(currentUser);
+
+      const response = await request(app)
+        .post("/api/v1/products")
+        .send(newProduct)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        errors: [{
+          message: "price must be greater than or equal to 0",
+          property: "price",
+        }]
+      });
+    });
+
+    test("should return 400 for missing required fields", async () => {
+      const newProduct = {
+        sku: "newsku",
+        name: "New Product",
+        // Missing description, price, stock, imageURL
+      };
+      const currentUser = await User.authenticate(username1, "password");
+      const token = createToken(currentUser);
+
+      const response = await request(app)
+        .post("/api/v1/products")
+        .send(newProduct)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        errors: [
+          { message: "requires property \"description\"", property: "description" },
+          { message: "requires property \"price\"", property: "price" },
+          { message: "requires property \"stock\"", property: "stock" },
+          { message: "requires property \"imageURL\"", property: "imageURL" }
+        ]
+      });
     });
   });
 });
