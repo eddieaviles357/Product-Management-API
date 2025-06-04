6fe171b8fb5b3d0d8db8220600ad30c7f9634a15
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-06-03 20:01:42 -0700

    imported User model and creatToken helper function to verify and create token for certain protected routes

--- __test__/controllers/reviews.test.js
+++ __test__/controllers/reviews.test.js
@@ -2,6 +2,8 @@
 const request = require("supertest");
 const app = require("../../app");
 const Reviews = require("../../models/Reviews");
+const User = require("../../models/Users");
+const createToken = require("../../helpers/tokens");
 const { BadRequestError } = require("../../AppError");
 const db = require("../../db.js");
 const {
@@ -72,6 +74,112 @@ describe("Reviews Controller", () => {
       });
     });
 
+    test("should return 500 on database error", async () => {
+      jest.spyOn(Reviews, "getReviewsForOneProduct").mockImplementation(() => {
+        throw new Error("Database error");
+      });
+
+      const response = await request(app)
+        .get(`/api/v1/reviews/product/${productIds[0]}`);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.body).toEqual({
+        error: {
+          message: "Database error",
+          status: 500
+        }
+      });
+    });
+  });
+
+  describe("GET /api/v1/reviews/product/:productId/:username", () => {
+    test("should return a single review", async () => {
+      const response = await request(app)
+        .get(`/api/v1/reviews/product/${productIds[0]}/${username1}`);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(response.body.review).toEqual({
+        productId: productIds[0],
+        userId: userIdUsername[0].id,
+        review: "nice item",
+        rating: 1,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      });
+    });
+
+    test("should return 400 if productId is not a number", async () => {
+      const response = await request(app)
+        .get("/api/v1/reviews/product/invalidId/username");
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        error: {
+          message: "Id must be a number",
+          status: 400
+        }
+      });
+    });
+
+    test("should return 404 if review does not exist", async () => {
+      const response = await request(app)
+        .get(`/api/v1/reviews/product/${productIds[0]}/nonexistentUser`);
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toEqual({
+        error: {
+          message: "User does not exist",
+          status: 404
+        }
+      });
+    });
+
+    test("should return 500 on database error", async () => {
+      jest.spyOn(Reviews, "getSingleReview").mockImplementation(() => {
+        throw new Error("Database error");
+      });
+
+      const response = await request(app)
+        .get(`/api/v1/reviews/product/${productIds[0]}/${username1}`);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.body).toEqual({
+        error: {
+          message: "Database error",
+          status: 500
+        }
+      });
+    });
+  });
+
+  describe("POST /api/v1/reviews/product/:productId/:username", () => {
+    test("should add a review to a product", async () => {
+    const currentUser = await User.authenticate(username1, "password");
+    const token = createToken(currentUser);
+      const newReview = {
+        review: "Great product!",
+        rating: 5
+      };
+
+      const response = await request(app)
+        .post(`/api/v1/reviews/product/${productIds[2]}/${username1}`)
+        .send(newReview)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body.success).toBe(true);
+      expect(response.body.review).toEqual({
+        productId: productIds[2],
+        userId: userIdUsername[0].id,
+        review: "Great product!",
+        rating: 5,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      });
+    });
+
+    
   });
 
 })
