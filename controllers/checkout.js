eabee4c838e6e96c68f890451db9caa37e89d83b
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-06-16 20:49:41 -0700

    getOrdersById route added, will retrieve order items

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -18,3 +18,21 @@ exports.createOrder = async (req, res, next) => {
     return next(err);
   }
 };
+
+// @desc      Get Order by ID
+// @route     GET /api/v1/checkout/:username/:orderId
+// @access    Private/Admin ?????????
+exports.getOrdersById = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    const order = await Orders.getOrderById(orderId);
+
+    if (!order || order.length === 0) {
+      throw new BadRequestError("Order not found");
+    }
+
+    return res.status(200).json({success: order});
+  } catch (err) {
+    return next(err);
+  }
+};
