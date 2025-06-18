a7c8ee475768e0a31a9d652eda1a1241246b68bc
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-06-17 18:55:22 -0700

    middleware included before hitting path

--- routes/checkout.js
+++ routes/checkout.js
@@ -8,15 +8,15 @@ const {
 } = require("../controllers/checkout");
 // const router = require("express").Router({mergeParams: true});
 const router = require("express").Router({mergeParams: true});
-const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth/auth");
+const { ensureLoggedIn, ensureUser, ensureUserOrAdmin } = require("../middleware/auth/auth");
 
 router
   .route('/:username')
-  .post(createOrder)
+  .post(ensureLoggedIn, ensureUser, createOrder)
 
 router
   .route('/:username/:orderId')
-  .get(getOrdersById);
+  .get(ensureLoggedIn, ensureUserOrAdmin, getOrdersById);
 
 module.exports = router;
 
