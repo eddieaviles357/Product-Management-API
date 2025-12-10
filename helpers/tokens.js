fade5ae2257abc8f5e3afae805fd6f753c6812cc
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-12-09 19:57:20 -0800

    imported crypto and promisify so we can convert jwt.sign into async

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -3,43 +3,41 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const { BadRequestError } = require("../AppError");
+const crypto = require("crypto");
+const { promisify } = require("util");
+
+const jwtSign = promisify(jwt.sign);
 
 // return signed JWT token from user data
-const createToken = (user) => {
+const createToken = async (user) => {
   console.assert(user?.isAdmin !== undefined, "create token passed without isAdmin property");
 
   if (!user || typeof user !== "object") {
-    return Promise.reject(new BadRequestError("User data is required to create a token"));
+    throw new BadRequestError("User data is required to create a token");
   }
 
   const { username, isAdmin } = user;
 
   if (!username) {
-    return Promise.reject(new BadRequestError("Username is required"));
+    throw new BadRequestError("Username is required");
   }
 
   if (isAdmin === undefined) {
-    return Promise.reject(new BadRequestError("isAdmin property is required"));
+    throw new BadRequestError("isAdmin property is required");
   }
   
   const payload = {
     username,
-    isAdmin: Boolean(isAdmin)
+    isAdmin: Boolean(isAdmin), // strict boolean
+    jti: crypto.randomUUID()   // Adds a unique token ID
   };
 
-  return new Promise((resolve, reject) => {
-    jwt.sign(
-      payload,
-      SECRET_KEY,
-      { expiresIn: "1h" },
-      (err, token) => {
-        if (err) {
-          return reject(new BadRequestError("Error creating token"));
-        }
-        resolve(token);
-      }
-    );
-  });
+  try {
+    const token = await jwtSign(payload, SECRET_KEY, { expiresIn: "1h" });
+    return token;
+  } catch (err) {
+    throw new BadRequestError("Error creating token");
+  }
 }
 
 module.exports = createToken;
