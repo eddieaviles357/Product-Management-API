18c55e1e5eddd36c8c53e8835f50905f0f71dc0b
Eduardo Aviles <eduardoaviles357@yahoo.com>
2026-01-12 20:04:18 -0800

    updated ssl option

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: getDatabaseUri(),
     ssl: {
-      rejectUnauthorized: false
+      rejectUnauthorized: true
     }
   });
 } else {
