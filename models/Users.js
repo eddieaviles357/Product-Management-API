2bca1ad69717678ab1c2170b9dfc69a5547c0f3c
Eduardo Aviles <eduardoaviles357@yahoo.com>
2026-01-20 18:47:41 -0800

    import createAndSendVerificaciton for sending verification email to user

--- models/Users.js
+++ models/Users.js
@@ -8,6 +8,7 @@ const {
   UnauthorizedError,
   ConflictError
 } = require("../AppError.js");
+const { createAndSendVerification } = require("../helpers/emailVerification");
 
 class Users {
   /** register user
@@ -28,18 +29,22 @@ class Users {
       }
       // create salt and hash password, we will store this in db
       const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-
+      
       const queryStatement = `INSERT INTO users (first_name, last_name, username, password, email, is_admin)
-                              VALUES 
-                                ($1, $2, $3, $4, $5, $6)
-                              RETURNING 
-                                id, first_name AS "firstName", last_name AS "lastName", username, email, is_admin AS "isAdmin"`;
+      VALUES 
+      ($1, $2, $3, $4, $5, $6)
+      RETURNING 
+      id, first_name AS "firstName", last_name AS "lastName", username, email, is_admin AS "isAdmin"`;
       const queryValues = [firstName, lastName, username, hashedPassword, email, isAdmin];
+      
       // hash password
       // store user in database
       const result = await db.query(queryStatement, queryValues);
       if(result.rows.length === 0) throw new BadRequestError('Invalid, something went wrong');
       
+      // Store verification token separately
+      const verificationResult = await createAndSendVerification({email, username});
+
       return result.rows[0];
       
     } catch (err) {
