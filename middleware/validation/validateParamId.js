6610b26d03b9b80ef146d8db7cd27985d4936171
Eduardo Aviles <eduardoaviles357@yahoo.com>
2026-03-10 21:45:02 -0700

    removed import that was not being used

--- middleware/validation/validateParamId.js
+++ middleware/validation/validateParamId.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { BadRequestError } = require("../../AppError");
+
 const validateParamId = (paramName) => (req, res, next) => {
   const value = Number(req.params[paramName]);
 
