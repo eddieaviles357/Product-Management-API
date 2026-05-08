2afb29f0c8765b57cd8deabc48f3727260db3961
Eduardo Aviles <eduardoaviles357@yahoo.com>
2026-05-07 21:47:36 -0700

    get productPrice query added, getOrderById and getAllOrdersByUsername updated to now include the snapshots of the users address and now the queries return a json object

--- helpers/sql/orderQueries.js
+++ helpers/sql/orderQueries.js
@@ -8,36 +8,68 @@ function getTotalAmount() {
   `;
 }
 
+function getProductPrice() {
+  return `
+    SELECT product_id, price
+    FROM products
+    WHERE product_id = ANY($1)
+    `;
+}
+
 function getOrderById() {
   return `
     SELECT O.id AS "orderId",
           O.status AS "orderStatus",
           O.total_amount::float AS "totalAmount",
-          A.address_1 AS "address1",
-          A.address_2 AS "address2", 
-          A.city,
-          A.state,
-          A.zipcode,
-          OP.product_id, 
-          OP.quantity,
-          P.id,
-          P."productName",
-          P."productDescription",
-          P.price AS "productPrice",
-          P."imageURL"
+          O.created_at AS "createdAt",
+          O.updated_at AS "updatedAt",
+
+          O.address_1 AS "address1",
+          O.address_2 AS "address2", 
+          O.city,
+          O.state,
+          O.zipcode,
+
+          json_agg(
+            json_build_object(
+              'productId', OP.product_id,
+              'quantity', OP.quantity,
+              'productName', P."productName",
+              'productDescription', P."productDescription",
+              'productPrice', P.price,
+              'imageURL', P."imageURL"
+            )
+          ) AS "orderItems"
+
     FROM orders O 
-    LEFT JOIN addresses A ON O.address_id = A.id
-    JOIN order_products OP ON O.id = OP.order_id
-    JOIN mv_product_list P ON OP.product_id = P.id
+
+    JOIN order_products OP 
+      ON O.id = OP.order_id
+
+    JOIN mv_product_list P 
+      ON OP.product_id = P.id
+
     WHERE O.id = $1
+
+    GROUP BY O.id
   `;
 }
 
+// old code - not used anymore
+// function insertIntoOrders() {
+//   return `
+//     INSERT INTO orders(user_id, total_amount, address_id) 
+//     VALUES($1, $2, $3) 
+//     RETURNING id, user_id AS "userId", total_amount::float AS "totalAmount", address_id AS "addressId", status
+//   `;
+// }
+
 function insertIntoOrders() {
   return `
-    INSERT INTO orders(user_id, total_amount, address_id) 
-    VALUES($1, $2, $3) 
-    RETURNING id
+    INSERT INTO orders(user_id, address_1, address_2, city, state, zipcode, total_amount) 
+    VALUES($1, $2, $3, $4, $5, $6, $7) 
+    RETURNING id, user_id AS "userId", total_amount::float AS "totalAmount", 
+              address_1 AS "address1", address_2 AS "address2", city, state, zipcode, status
   `;
 }
 
@@ -59,28 +91,43 @@ function insertIntoOrderProducts(placeholders) {
 
 function getAllOrdersByUsername() {
   return `
-    SELECT O.id AS "orderId", 
-          O.total_amount::float AS "totalAmount", 
-          O.status AS "orderStatus",
-          O.created_at AS "createdAt",
-          A.address_1 AS "address1",
-          A.address_2 AS "address2", 
-          A.city,
-          A.state,
-          A.zipcode,
-          OP.product_id, 
-          OP.quantity, 
-          OP.total_amount::float AS "productTotalAmount",
-          P.id,
-          P."productName",
-          P."productDescription",
-          P.price AS "productPrice",
-          P."imageURL"
+    SELECT 
+      O.id AS "orderId", 
+      O.status AS "orderStatus",
+      O.total_amount::float AS "totalAmount", 
+      O.created_at AS "createdAt",
+      O.updated_at AS "updatedAt",
+
+      O.address_1 AS "address1",
+      O.address_2 AS "address2",
+      O.city,
+      O.state,
+      O.zipcode,
+
+      json_agg(
+        json_build_object(
+          'productId', OP.product_id,
+          'quantity', OP.quantity,
+          'productTotalAmount', OP.total_amount::float,
+          'productName', P."productName",
+          'productDescription', P."productDescription",
+          'productPrice', P.price,
+          'imageURL', P."imageURL"
+        )
+      ) AS "orderItems"
+
     FROM orders O
-    LEFT JOIN addresses A ON O.address_id = A.id
-    JOIN order_products OP ON O.id = OP.order_id
-    JOIN mv_product_list P ON OP.product_id = P.id
+
+    JOIN order_products OP 
+      ON O.id = OP.order_id
+
+    JOIN mv_product_list P 
+      ON OP.product_id = P.id
+
     WHERE O.user_id = $1
+
+    GROUP BY O.id
+
     ORDER BY O.created_at DESC
   `;
 };
@@ -90,6 +137,7 @@ module.exports = {
   insertIntoOrderProducts,
   getTotalAmount,
   getOrderById,
+  getProductPrice,
   getAllOrdersByUsername,
   updateOrders,
 }
